2c0756b34b86aa5c1caf41e48a30ef02971698d1
Gabriel Brum <gbrb1@hotmail.com>
2026-08-29 07:06:08 -0300

    feat: add ticket priority

--- backend/HelpDesk.Domain/Entities/Ticket.cs
+++ backend/HelpDesk.Domain/Entities/Ticket.cs
@@ -7,12 +7,13 @@ public class Ticket
     public int Id { get; private set; }
     public string Title { get; private set; }
     public string Description { get; private set; }
-
     public TicketStatus Status { get; private set; }
-
+    public TicketPriority Priority { get; private set; }
     public Ticket(string title, string description)
     {
         Title = title;
         Description = description;
+        Status = TicketStatus.Open;
+        Priority = TicketPriority.Medium;
     }
 }
