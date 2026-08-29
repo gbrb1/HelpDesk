6dbe9289d19428f2ab371cc4886cba9beff8441d
Gabriel Brum <gbrb1@hotmail.com>
2026-08-29 08:03:00 -0300

    add ticket repository

---
+++ HelpDesk.Infrastructure/Repositories/TicketRepository.cs
@@ -0,0 +1,32 @@
+﻿using HelpDesk.Application.Interfaces;
+using HelpDesk.Domain.Entities;
+
+namespace HelpDesk.Infrastructure.Repositories;
+
+public class TicketRepository : ITicketRepository
+{
+    public Task<Ticket?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Ticket>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Ticket ticket)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Ticket ticket)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Ticket ticket)
+    {
+        throw new NotImplementedException();
+    }
+}
